4e347ff962045fde6ddbd5280a4d2c356dcccbcd
phibh <phibh96@gmail.com>
2025-03-04 22:12:24 +0700

    add tour guide list

--- SourceCode/capstone-project/src/main/java/com/example/demo/HomeController.java
+++ SourceCode/capstone-project/src/main/java/com/example/demo/HomeController.java
@@ -3,25 +3,30 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
     
     @GetMapping("/")
     public String home(Model model) {
-        //model.addAttribute("name", "Phi"); // Passing data to Thymeleaf
         return "home";
     }
    
     @GetMapping("/login")
     public String login(Model model) {
-        //model.addAttribute("name", "Phi"); // Passing data to Thymeleaf
         return "login";
     }
     
     @GetMapping("/signup")
     public String signup(Model model) {
-        //model.addAttribute("name", "Phi"); // Passing data to Thymeleaf
         return "signup";
     }
+    
+    @GetMapping("/tour-guides")
+    public String tourGuides(Model model, @RequestParam(defaultValue = "all") String city) {
+    	System.out.println("City = " + city);
+        return "tour-guides";
+    }
+    
 }
