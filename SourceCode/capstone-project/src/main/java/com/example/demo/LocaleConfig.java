3c90cf95efa963f4f037cbaaf204fd53a0809d8b
phibh <phibh96@gmail.com>
2025-03-05 20:21:37 +0700

    fix multi language issue

--- SourceCode/capstone-project/src/main/java/com/example/demo/LocaleConfig.java
+++ SourceCode/capstone-project/src/main/java/com/example/demo/LocaleConfig.java
@@ -8,9 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -21,7 +19,7 @@ public class LocaleConfig implements WebMvcConfigurer{
 	public LocaleResolver localeResolver() {
 		System.out.println("localeResolver");
 		SessionLocaleResolver resolver = new SessionLocaleResolver();
-		resolver.setDefaultLocale(new Locale("vi"));
+		resolver.setDefaultLocale(new Locale("vn"));
 		resolver.setDefaultTimeZone(TimeZone.getTimeZone("UTC"));
 		return resolver;
 	}
