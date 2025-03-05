fb14b3821a6bae16263212ba13dc10bb53746ce2
phibh <phibh96@gmail.com>
2025-03-05 16:00:10 +0700

    support multi languages

--- SourceCode/capstone-project/src/main/java/com/example/demo/CapstoneProjectApplication.java
+++ SourceCode/capstone-project/src/main/java/com/example/demo/CapstoneProjectApplication.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
 
 @SpringBootApplication
 public class CapstoneProjectApplication {
---
+++ SourceCode/capstone-project/src/main/java/com/example/demo/LocaleConfig.java
@@ -0,0 +1,42 @@
+
+package com.example.demo;
+
+import java.util.Locale;
+import java.util.TimeZone;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer{
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		System.out.println("localeResolver");
+		SessionLocaleResolver resolver = new SessionLocaleResolver();
+		resolver.setDefaultLocale(new Locale("vi"));
+		resolver.setDefaultTimeZone(TimeZone.getTimeZone("UTC"));
+		return resolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		System.out.println("localeChangeInterceptor");
+		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();		
+		interceptor.setParamName("localeData"); // Change language via URL param
+		return interceptor;
+	}
+	
+	@Override
+    public void addInterceptors(InterceptorRegistry registry) {
+		System.out.println("addInterceptors");
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
